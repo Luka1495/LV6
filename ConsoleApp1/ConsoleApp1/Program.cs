9eb34717db3b6e7133229365de96d41e4dfbd389
Luka1495 <62608325+Luka1495@users.noreply.github.com>
2020-05-14 13:12:22 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook();
+            notebook.AddNote(new Note("rppoon", "Luka Vukadin"));
+            notebook.AddNote(new Note("rppoon", "Tekst"));
+            notebook.AddNote(new Note("Naslov", "bok"));
+
+            Iterator iterator = new Iterator(notebook);
+            iterator.Current.Show();
+            iterator.Next().Show();
+
+            while (iterator.IsDone != true)
+            {
+                iterator.Current.Show();
+                iterator.Next();
+            }
+        }
+    }
+}
---
+++ LV6_2/LV6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            box.AddProduct(new Product("Rumenko", 2.50));
+            box.AddProduct(new Product("King", 6.00));
+            box.AddProduct(new Product("Njofra", 4.00));
+
+            BoxIterator iterator = new BoxIterator(box);
+            
+            while (iterator.isDone == false)
+            {
+                Console.WriteLine(iterator.Current.ToString());
+                iterator.Next();
+            }
+        }
+    }
+}
